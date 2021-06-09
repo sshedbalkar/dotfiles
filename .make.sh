1515359c699395bddcebb5a92cdc2f4abda84445
Santosh Shedbalkar <sanoysyg@gmail.com>
2021-06-10 02:54:45 +0530

    Updated .make.sh file to check and create/recreate symlinks only for missing/broken links

--- .make.sh
+++ .make.sh
@@ -14,6 +14,26 @@ array=(".make.sh" ".git" ".DS_Store" ".gitignore" "README.md")
 
 ##########
 
+FnCreateSymlink () {
+    my_link=~/$1
+    if [ -L ${my_link} ] ; then
+       if [ -e ${my_link} ] ; then
+          echo "Link exists: $my_link"
+       else
+          echo "Broken link, recreating: $my_link"
+          rm $my_link
+          ln -s $dir/$1 $my_link
+       fi
+    elif [ -e ${my_link} ] ; then
+       echo "Not a link, recreating: $my_link"
+       rm $my_link
+       ln -s $dir/$1 $my_link
+    else
+       echo "Missing, creating new: $my_link"
+       ln -s $dir/$1 $my_link
+    fi
+}
+
 # change to the dotfiles directory
 echo "Changing to the $dir directory"
 cd $dir
@@ -21,11 +41,13 @@ cd $dir
 for entry in * .[^.]*; do
     if [[ ! "${array[@]}" =~ "$entry" ]]; then
         if [[ -d $entry ]]; then
-            echo "Creating symlink for directory: $entry"
-            ln -s $dir/$entry ~/$entry
+            # echo "Creating symlink for directory: $entry"
+            # ln -s $dir/$entry ~/$entry
+            FnCreateSymlink "$entry"
         elif [[ -f $entry ]]; then
-            echo "Creating symlink for file: $entry"
-            ln -s $dir/$entry ~/$entry
+            # echo "Creating symlink for file: $entry"
+            # ln -s $dir/$entry ~/$entry
+            FnCreateSymlink "$entry"
         else
             echo "$entry is invalid"
             exit 1
